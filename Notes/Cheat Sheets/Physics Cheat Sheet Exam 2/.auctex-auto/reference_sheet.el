ddb2262e5a0bf058d92a017724288d72e494f3c2
johnnyarojas-academic <johnny.rojas.k@gmail.com>
2026-02-12 13:41:37 -0500

    Started Equation Sheet for Exam 2

---
+++ Notes/Cheat Sheets/Physics Cheat Sheet Exam 2/.auctex-auto/reference_sheet.el
@@ -0,0 +1,22 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "reference_sheet"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt" "twocolumn")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("geometry" "") ("amsmath" "") ("amssymb" "") ("graphicx" "") ("booktabs" "") ("enumitem" "")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "inputenc"
+    "geometry"
+    "amsmath"
+    "amssymb"
+    "graphicx"
+    "booktabs"
+    "enumitem"))
+ :latex)
+
